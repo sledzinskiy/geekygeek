b750e5e0832a989eb3f1d4a83ff4a5afeb088f41
andrey <andrey@andrey-X550LB.(none)>
2014-11-02 13:04:09 +0200

    - Created static html files - Added mapping to static pages in urls.py - Added rendering to views.py

--- KolyAndron/urls.py
+++ KolyAndron/urls.py
@@ -4,7 +4,15 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
      url(r'^home/', 'KolyAndron.view.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     url(r'^$', 'KolyAndron.view.home', name='home'),
+     url(r'^myprofile/', 'KolyAndron.view.myprofile', name='myprofile'),
+     url(r'^groups/', 'KolyAndron.view.groups', name='groups'),
+     url(r'^groups/(\d{1,2})/', 'KolyAndron.view.group', name='group'),
+     url(r'^geeks/', 'KolyAndron.view.geeks', name='geeks'),
+     url(r'^news/', 'KolyAndron.view.news', name='news'),
+     url(r'^news/(\d{1,2})/', 'KolyAndron.view.newsdetail', name='newsdetail'),
+     url(r'^about/', 'KolyAndron.view.about', name='about'),
+     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- KolyAndron/view.py
+++ KolyAndron/view.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render_to_response
 
+
 def home(request):
-		return render_to_response('home.html')
+    return render_to_response('home.html')
+
+
+def myprofile(request):
+    return render_to_response('myprofile.html')
+
+
+def groups(request):
+    return render_to_response('groups.html')
+
+
+def group(request):
+    return render_to_response('group.html')
+
+
+def geeks(request):
+    return render_to_response('geeks.html')
+
+
+def news(request):
+    return render_to_response('news.html')
+
+
+def newsdetail(request):
+    return render_to_response('newsdetail.html')
+
+
+def about(request):
+    return render_to_response('about.html')
